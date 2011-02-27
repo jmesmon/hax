b8a488af4d4f955350e48d17f0d8e9ea215c9ae1
Cody Schafer <cpschafer@gmail.com>
2011-02-27 16:40:36 -0500

    simple: use better WARN.

--- simple/prog_conf.h
+++ simple/prog_conf.h
@@ -5,8 +5,12 @@
  * determine how the uC should be configured */
 #include <stdio.h>
 
-
 #define PROG_ANALOG_IN_NUM 16
-#define WARN(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#define WARN(fmt, ...) _WARN(__FILE__,__LINE__,fmt, ##__VA_ARGS__)
+#define _WARN(file, line, fmt, ...) do { \
+	fputs(#file ":" #line ":", stdout); \
+	printf(fmt, ##__VA_ARGS__); \
+} while(0)
+
 
 #endif
